25f5ae2c284f1994d2e32c5732e38a1dc226aee6
inbreeze <junjie2017@yeah.net>
2022-04-18 18:35:03 +0800

    refactor: 修补 CodeStyle

--- src/test/scala/BreakTest.scala
+++ src/test/scala/BreakTest.scala
@@ -1,12 +1,14 @@
+package scala
+
 import scala.util.control.Breaks._
 
 /**
  * @Project Leetcode
  * @Author INBreeze
  * @Date 2020/7/31 9:49
- * @Description
  */
-object breakTest {
+// noinspection ScalaDocParserErrorInspection,ScalaDocUnknownTag
+object BreakTest {
   def main(args: Array[String]): Unit = {
     // 控制守卫 语法糖
     for (i <- 1 to 10 if i != 5) {
